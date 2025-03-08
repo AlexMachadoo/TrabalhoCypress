2f7a563ea7b354fe0e076f6a9f2989a86a8764e9
AlexMachadoo <barros.machado@hotmail.com>
2025-03-07 23:30:23 -0300

    Teste Cypress finalizado

--- cypress/e2e/register.spec.cy.js
+++ cypress/e2e/register.spec.cy.js
@@ -1,8 +1,9 @@
 describe('Formulário', () => {
 
-  it('Deve acessar  página do formulário', () => {
+  it('Deve acessar a página do formulário', () => {
     cy.visit("https://alexmachadoo.github.io/TrabalhoCypress/");
-
+    cy.wait(3000); // Espera pra garantir o carregamento
+    cy.get('#nome', { timeout: 10000 }).should('exist'); // Verifica se o elemento tá lá mesmo
   });
 
   it('Preencher os campos corretamente', () => {
@@ -11,18 +12,16 @@ describe('Formulário', () => {
     cy.get('#telefone').type('4299999999');
     cy.get('#senha').type('senha963');
     cy.get('#confirma_senha').type('senha963');
-  });
 
-  cy.get('#nome').should('have.value', 'Alex Sandro Machado');
-  cy.get('#email').should(',have.value','alex.s@email.com');
-  cy.get('#telefone').should('4299999999');
-  cy.get('#senha').should('have.value','senha963');
-  cy.get('#confirma_senha').should('have.value','senha963');
+    cy.get('#nome').should('have.value', 'Alex Sandro Machado');
+    cy.get('#email').should('have.value', 'alex.s@email.com');
+    cy.get('#telefone').should('have.value', '4299999999');
+    cy.get('#senha').should('have.value', 'senha963');
+    cy.get('#confirma_senha').should('have.value', 'senha963');
   });
 
   it('Submeter o formulário com sucesso', () => {
     cy.get('form#cadastro').submit();
-
     cy.get('form#cadastro').should('exist');
-
+  });
 });
