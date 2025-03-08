a0893817906445c6621acece5e35bda233b79557
AlexMachadoo <barros.machado@hotmail.com>
2025-03-07 23:17:14 -0300

    Primeiro commit

---
+++ cypress/e2e/register.spec.cy.js
@@ -0,0 +1,27 @@
+describe('Formulário', () => {
+
+  it('Deve acessar  página do formulário', () => {
+
+  });
+
+  it('Preencher os campos corretamente', () => {
+    cy.get('#nome').type('Alex Sandro Machado');
+    cy.get('#email').type('alex.s@email.com');
+    cy.get('#telefone').type('4299999999');
+    cy.get('#senha').type('senha963');
+    cy.get('#confirma_senha').type('senha963');
+  });
+
+  cy.get('#nome').shuold('have.value', 'Alex Sandro Machado');
+  cy.get('#email').should(',have.value','alex.s@email.com');
+  cy.get('#telefone').should('4299999999');
+  cy.get('#senha').should('have.value','senha963');
+  cy.get('#confirma_senha').should('have.value','senha963');
+  });
+
+  it('Submeter o formulário com sucesso', () => {
+    cy.get('form#cadastro').submit();
+
+    cy.get('form#cadastro').should('exist');
+
+});
