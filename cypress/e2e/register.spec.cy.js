f77d645ea6ae839c5319b1dc793c49f0246f244b
AlexMachadoo <barros.machado@hotmail.com>
2025-03-07 23:58:28 -0300

    Teste Cypress finalizado

--- cypress/e2e/register.spec.cy.js
+++ cypress/e2e/register.spec.cy.js
@@ -1,26 +1,42 @@
 describe('Formulário', () => {
+  beforeEach(() => {
+    cy.visit("https://alexmachadoo.github.io/TrabalhoCypress/");
+  });
 
   it('Deve acessar a página do formulário', () => {
-    cy.visit("https://alexmachadoo.github.io/TrabalhoCypress/");
-    
+    cy.url().should('include', 'TrabalhoCypress');
   });
 
   it('Preencher os campos corretamente', () => {
-    cy.get('#nome').type('João Silva');
-    cy.get('#email').type('joao.silva@email.com');
-    cy.get('#telefone').type('41199999999');
-    cy.get('#senha').type('Teste@123');
-    cy.get('#confirma_senha').type('Teste@123');
+    cy.get('[name="nome"]').type('João Silva');
+    cy.get('[name="email"]').type('joao.silva@email.com');
+    cy.get('[name="telefone"]').type('4119999999');
+    cy.get('[name="senha"]').type('Teste@123');
+    cy.get('[name="confirma_senha"]').type('Teste@123');
 
-    cy.get('#nome').should('have.value', 'João Silva');
-    cy.get('#email').should('have.value', 'joao.silva@email.com');
-    cy.get('#telefone').should('have.value', '1199999999');
-    cy.get('#senha').should('have.value', 'Teste@123');
-    cy.get('#confirma_senha').should('have.value', 'Teste@123');
+    cy.get('[name="nome"]').should('have.value', 'João Silva');
+    cy.get('[name="email"]').should('have.value', 'joao.silva@email.com');
+    cy.get('[name="telefone"]').should('have.value', '4119999999');
+    cy.get('[name="senha"]').should('have.value', 'Teste@123');
+    cy.get('[name="confirma_senha"]').should('have.value', 'Teste@123');
+  });
+
+  it('Validar erro do telefone com menos de 10 dígitos', () => {
+    cy.get('[name="telefone"]').clear().type('411999999');
+    cy.get('.erro-telefone').should('be.visible');
+    cy.get('.erro-telefone').should('have.text', 'Telefone inválido');
   });
 
   it('Submeter o formulário com sucesso', () => {
-    cy.get('form#cadastro').submit();
-    cy.get('form#cadastro').should('exist');
+    cy.get('[name="nome"]').type('João Silva');
+    cy.get('[name="email"]').type('joao.silva@email.com');
+    cy.get('[name="telefone"]').type('4119999999');
+    cy.get('[name="senha"]').type('Teste@123');
+    cy.get('[name="confirma_senha"]').type('Teste@123');
+
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.mensagem-sucesso').should('be.visible');
+    cy.get('.mensagem-sucesso').should('have.text', 'Cadastro realizado com sucesso');
   });
 });
